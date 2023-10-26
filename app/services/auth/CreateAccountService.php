ea4f6c43c7170526d75676a06cfaf382de946ba4
level237 <Bramslevel129@gmail.com>
2023-10-26 03:26:55 +0100

    Solid principle:account user structure folder

--- app/services/auth/CreateAccountService.php
+++ app/services/auth/CreateAccountService.php
@@ -6,6 +6,7 @@ use App\Interface\auth\NewAccountInterface;
 
 class CreateAccountService implements NewAccountInterface{
 
+    // ici le scope est une chaine de caractere unique a chaque user
     public function createAccount(string $username, ?string $email = null, string $phone_number, string $password, string $scope)
     {
 
