721fa19dc34c8fd4c75ba6100c0bca61b92153b9
level237 <Bramslevel129@gmail.com>
2023-10-27 07:05:49 +0100

    login implementation

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,26 @@ class ClientSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'name'=>'user',
+                'secret' => "Ja3gbBhsdiEJKdZiNBHCbNURCsgeFL933ZLfIpVX",
+                'redirect' => "http://localhost/auth/callback",
+                'personal_access_client'=>0,
+                'password_client'=>0,
+                'revoked'=>0
+            ],
+            [
+                'name'=>'Laravel Password Grant Client',
+                'secret' => "JEqpOXFF3Bgt3ND2UzbnuBjuhgIOtGGFJwb377rl",
+                "provider"=>"users",
+                'redirect' => "http://localhost/",
+                'personal_access_client'=>0,
+                'password_client'=> 1,
+                'revoked'=>0
+            ],
+        ];
+
+        DB::table('oauth_clients')->insert($data);
     }
 }
