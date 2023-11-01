2cc4d398071a4cfe86911b49e52672cac9a00943
Frank <delanofofe@gmail.com>
2023-11-01 12:37:21 +0100

    Google recaptcha validated

--- app/Http/Controllers/CreateUserController.php
+++ app/Http/Controllers/CreateUserController.php
@@ -7,12 +7,42 @@ use App\Http\Requests\UserRequest;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\IpUtils;
 
 class CreateUserController extends Controller
 {
 
     public function createUser(UserRequest $request){
        
+
+        $recaptcha_response = $request->input('recaptcha');
+        
+
+        if (is_null($recaptcha_response)) {
+    
+             return response("{error:Please Complete the Recaptcha to proceed}", 400)
+                  ->header('Content-Type', 'application/json');
+        }
+
+        $url = "https://www.google.com/recaptcha/api/siteverify";
+
+        $body = [
+            'secret' => config('services.recaptcha.secret'),
+            'response' => $recaptcha_response,
+            'remoteip' => IpUtils::anonymize($request->ip()) //anonymize the ip to be GDPR compliant. Otherwise just pass the default ip address
+        ];
+
+        $response = Http::asForm()->post($url, $body);
+
+        $result = json_decode($response);
+
+        if (!$response->successful() && $result->success == false) {
+
+           return response("{error:Please Complete the Recaptcha Again to proceed}", 400)
+                  ->header('Content-Type', 'application/json');
+
+        } 
 
         $validatedData=$request->validated();
 
