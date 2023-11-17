fa3663e8f1a6bf0333876fa83ffdf7fb91cbc25f
level237 <Bramslevel129@gmail.com>
2023-11-17 10:57:21 +0100

    Update

--- app/Http/Controllers/Api/Admin/NewUserController.php
+++ app/Http/Controllers/Api/Admin/NewUserController.php
@@ -16,12 +16,16 @@ class NewUserController extends Controller
         try{
             $user = new User;
             $user->username = $userRequest->username;
+            if(isset($userRequest->email)){
+                $user->email = $userRequest->email;
+            }
+
             $user->role_id = $userRequest->role_id;
             $user->phone_number = $userRequest->phone_number;
             $user->password = Hash::make($userRequest->password);
 
             if($user->save()){
-                return response()->json(['user'=>$user],200);
+                return response()->json(['status'=>200,'user'=>$user],200);
             }
         }catch(Exception $e){
  return response()->json([
