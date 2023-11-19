fa1ac0059abfb5165176fb6b3ca2d22ebb1d0346
level237 <Bramslevel129@gmail.com>
2023-11-19 13:38:12 +0100

    update

--- database/seeders/CreditSeeder.php
+++ database/seeders/CreditSeeder.php
@@ -1,51 +1,34 @@
 <?php
 
-/**
- * Seeder for MemberShip Table
- * Created on 25 October 2023
- * Author : Frank Zohim
-*/
-
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-class MemberShipSeeder extends Seeder
+class CreditSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-         //Data to store
         $data = [
             [
-                'membership_name'=>'Premium',
-                'period'=>'30',
-                'price' => '3000',
-                'credit_id'=>1,
+                'credit'=>'3000',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
 
               [
-                'membership_name'=>'GOLD',
-                'period'=>'90',
-                'price' => '5000',
-                'credit_id'=>2,
+                'credit'=>'5000',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
 
               [
-                'membership_name'=>'Extra-vip',
-                'period'=>'90',
-                'price' => '10.000',
-                'credit_id'=>3,
+                'credit'=>'10000',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
