4e5fbbcd524edd6efd0e2ce91aa80d2d7185f89e
level237 <Bramslevel129@gmail.com>
2023-12-03 10:47:44 +0100

    profile visit:update

---
+++ app/Models/Profile_visit.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile_visit extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'escort_id'
+    ];
+
+}
