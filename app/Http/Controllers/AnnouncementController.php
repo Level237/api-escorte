96794269c7e249a34c908722f44534f02d3c77e4
level237 <Bramslevel129@gmail.com>
2023-12-30 15:35:44 +0100

    update

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -114,7 +114,7 @@ class AnnouncementController extends Controller
 
     public function displayAdsImage($id, $path)
     {
-        $image = Image::make(storage_path('app\public\ads\\'.$id.'\\'. $path));
+        $image = Image::make(storage_path('app/public/ads/'.$id.'/'. $path));
         $i=Image::make(public_path("logo.png"));
         $i->resize(300, 300);
         $i->blur();
