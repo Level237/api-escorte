c1703631770c9a5b56d6998122400117290c2800
level237 <Bramslevel129@gmail.com>
2024-01-16 11:25:10 +0100

    increase format

--- app/Http/Controllers/Api/Admin/StatController.php
+++ app/Http/Controllers/Api/Admin/StatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Admin;
 
+use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,14 +10,36 @@ use App\Http\Controllers\Controller;
 class StatController extends Controller
 {
     public function users(){
-        $users=User::where('suspended_at',"!=",null)->count();
+        $users=User::where('suspended_at',"=",null)->count();
 
         return $users;
+
     }
 
     public function escorts(){
-        $escorts=User::where('role_id',2)->count();
 
-        return $escorts;
+        $dateFrom = Carbon::now()->subDays(30);
+        $dateTo = Carbon::now();
+        $percent_from="";
+        $monthly = User::where('role_id',2)->whereBetween('created_at', [$dateFrom, $dateTo])->count();
+        $previousDateFrom = Carbon::now()->subDays(60);
+        $previousDateTo = Carbon::now()->subDays(31);
+        $previousMonthly = User::where('role_id',2)->whereBetween('created_at', [$previousDateFrom, $previousDateTo])->count();
+        if ($previousMonthly < $monthly) {
+            if ($previousMonthly > 0) {
+                $percent_from = $monthly - $previousMonthly;
+                (int)  $percent = ($previousMonthly * 100)/$percent_from ; //increase percent
+                return response()->json(['monthly'=>$monthly,'percent'=>number_format($percent)]);
+            } else {
+                (int) $percent = 100;
+
+            }
+        } else {
+            $percent_from = $previousMonthly - $monthly;
+            (int) $percent = $percent_from / $previousMonthly * 100;
+            return $percent;
+        }
+
+        return $previousMonthly;
     }
 }
