876880e5e2928db9dd18693d4b01380319f54eec
level237 <Bramslevel129@gmail.com>
2024-01-17 09:15:04 +0100

    update

--- database/seeders/PaymentSeeder.php
+++ database/seeders/PaymentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -19,101 +20,141 @@ class PaymentSeeder extends Seeder
                 'payment_type'=>"Momo",
                 'price'=>3000,
                 'user_id'=>5,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>3000,
                 'user_id'=>6,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>5000,
                 'user_id'=>7,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>10000,
                 'user_id'=>8,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>3000,
                 'user_id'=>9,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>3000,
                 'user_id'=>10,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>5000,
                 'user_id'=>11,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>5000,
                 'user_id'=>12,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>10000,
                 'user_id'=>13,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>10000,
                 'user_id'=>14,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>3000,
                 'user_id'=>15,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>3000,
                 'user_id'=>16,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>10000,
                 'user_id'=>17,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>3000,
                 'user_id'=>8,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>5000,
                 'user_id'=>19,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>10000,
                 'user_id'=>20,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>3000,
                 'user_id'=>21,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>5000,
                 'user_id'=>5,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>10000,
                 'user_id'=>6,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'payment_type'=>"Momo",
                 'price'=>3000,
                 'user_id'=>7,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
 
         ];
